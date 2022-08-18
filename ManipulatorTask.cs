4bef0870515377fdfe8e354920228aeab5dbefb2
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-08-18 16:34:01 +0300

    Untested completed code. Need add class PointD with values of type double so that the tests passes. Although need add random testing which I don't know as realised

--- ManipulatorTask.cs
+++ ManipulatorTask.cs
@@ -1,13 +1,11 @@
 using System;
+using System.Drawing;
 using NUnit.Framework;
 
 namespace Manipulation
 {
     public static class ManipulatorTask
     {
-        const float maxLengthManipulator = Manipulator.Forearm
-                + Manipulator.Palm + Manipulator.UpperArm;
-
         /// <summary>
         /// Возвращает массив углов (shoulder, elbow, wrist),
         /// необходимых для приведения эффектора манипулятора в точку x и y 
@@ -16,13 +14,37 @@ namespace Manipulation
         /// </summary>
         public static double[] MoveManipulatorTo(double x, double y, double alpha)
         {
-            // Используйте поля Forearm, UpperArm, Palm класса Manipulator
+            var wristPos = AnglesToCoordinatesTask
+                .GetPointPosition(Math.PI - alpha, Manipulator.Palm,
+                new PointF((float)x, (float)y));
+            var shoulderWristLength = GetLengthFromStartCoordinate
+                (wristPos.X, wristPos.Y);
+            if (!TriangleTask.IsDegenerateTriangle(
+                Manipulator.Forearm, Manipulator.UpperArm,
+                shoulderWristLength))
+            {
+                var elbow = TriangleTask.GetABAngle(a: Manipulator.Forearm,
+                    b: Manipulator.UpperArm,
+                    c: shoulderWristLength);
+                var firstPieceOfShoulder = TriangleTask.GetABAngle
+                    (a: Manipulator.UpperArm,
+                    b: shoulderWristLength,
+                    c: Manipulator.Forearm);
+                var secondPieceOfShoulder = Math.Atan2(wristPos.Y, wristPos.X);
+                var shoulder = firstPieceOfShoulder + secondPieceOfShoulder;
+                var wrist = -alpha - shoulder - elbow;
+                if (shoulder != double.NaN && elbow != double.NaN && wrist != double.NaN)
+                    return new[] { shoulder, elbow, wrist };
+            }
             return new[] { double.NaN, double.NaN, double.NaN };
         }
 
-        private static bool CanMove(double xWrist, double yWrist)
-        => Math.Sqrt(xWrist * xWrist + yWrist * yWrist) < maxLengthManipulator;
+        public static double GetLengthFromStartCoordinate
+            (double x, double y)
+        => Math.Sqrt(x * x + y * y);
     }
+
+
 
     [TestFixture]
     public class ManipulatorTask_Tests
@@ -30,6 +52,15 @@ namespace Manipulation
         [Test]
         public void TestMoveManipulatorTo()
         {
+            double expectedTriangle1 = 0;
+            double expectedTriangle2 = 0;
+            double expectedTriangle3 = 0;
+            Random random = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                //var actualTriangles = ManipulatorTask
+                //    .MoveManipulatorTo()
+            }
             //Assert.Fail("Write randomized test here!");
         }
     }
